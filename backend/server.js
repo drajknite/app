c7eaf8bc88d833be3914562f4bfcab6b56ae1a33
drajknite <deepanr@infinite.com>
2025-07-15 10:59:57 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,22 @@ const PORT = process.env.PORT || 5000;
 Db name: app
 Collections: users, notes
 use functions getUsersCollection() or getNotesCollection() to access collections
+sample users
+[
+    {
+      "index": 0,
+      "email": "goldie_weber@marketoid.mortgage",
+      "username": "goldie01",
+      "name": "Goldie Weber",
+      "firstName": "Bass",
+      "lastName": "Nelson",
+      "company": "Marketoid",
+      "dob": "2001-10-29",
+      "address": "72 Keen Court, Broadlands, Montana",
+      "password": "",
+      "isActive": true
+    }
+]
 */
 
 connectToMongo().then(() => {
